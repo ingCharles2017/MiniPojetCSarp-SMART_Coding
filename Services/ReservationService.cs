2ec694bd6e2d4ae4c108a4176b5eacb4c1ea56e3
Delva Patrick <delvapatrick25@gmail.com>
2026-01-07 22:19:27 -0500

    Ajouter la methode Pour pouvoir afficher la listes des reservations

--- Services/ReservationService.cs
+++ Services/ReservationService.cs
@@ -2,26 +2,25 @@ using System.Collections.Generic;
 using System.Linq;
 using GestionReservations.Models;
 
-
-namespace ReservationService {
-
+namespace GestionReservations.Services
+{
     public class ReservationService
     {
         private List<Reservation> reservations = new List<Reservation>();
 
         public void CreerReservation(Reservation reservation)
         {
-            reservation.Add(reservation);
-
+            reservations.Add(reservation);
         }
 
-        public Reservation ObtenirParId(int Id)
+        public Reservation ObtenirParId(int id)
         {
             return reservations.FirstOrDefault(r => r.Id == id);
         }
 
+        public List<Reservation> ListerReservations()
+        {
+            return reservations;
         }
-
-
-
+    }
 }
