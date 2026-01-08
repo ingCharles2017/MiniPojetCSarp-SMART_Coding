b4fafa01e7d07af1d1a9022c38ed16d8db58b0ef
Delva Patrick <delvapatrick25@gmail.com>
2026-01-07 21:56:30 -0500

    Creation du constructeur dans la classe reservation

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -11,5 +11,14 @@ namespace GestionReservations.Models
         public Client ClientInterne { get; set; }
         public DateTime DateReservation { get; set; }
         public string Statut { get; set; }
+
+        public Reservation(int id, Ressource ressource, Personne client, DateTime date)
+        {
+            Id = id;
+            Ressource = ressource;
+            ClientInterne = client;
+            DateReservation = date;
+            Statut = "Confirmée";
+        }
     }
 }
