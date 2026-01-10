c0de204abf31734ebabee32398cfd7e8a58ddfe1
Delva Patrick <delvapatrick25@gmail.com>
2026-01-09 21:09:52 -0500

    Initiation de la logique dans Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Transactions;
+using Reservation.Models;
+using RessourceService.Services;
 
-namespace MyApp
+
+namespace GestionReservations
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+
+        static void Main(string[] Args)
         {
-            Console.WriteLine("Hello World!");
+
+            ResourceService resourceService = new ResourceService();
+            ReservationService reservationService = new ReservationService();
+
+            InitialiserDonnees(resourceService);
+
+            bool quitter = false;
+
+            while (!quitter)
+            {
+                Console.Clear();
+                AfficherMenuPrincipal();
+                Console.Write("Votre choix : ");
+                string choix = Console.ReadLine();
+
+                switch (choix)
+                {
+                    case "1":
+                        ListerRessources(resourceService);
+                        break;
+
+                    case "2":
+                        CreerReservation(resourceService, reservationService);
+                        break;
+
+                    case "3":
+                        ListerReservations(reservationService);
+                        break;
+
+                    case "0":
+                        quitter = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Choix invalide.");
+                        Pause();
+                        break;
+                }
+            }
         }
+
     }
+    
+
+
 }
+
